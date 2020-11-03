b99bdd8871457ec5a1037202902e612807cf6083
Gareth Williams <garethrwilliams@me.com>
2020-11-03 12:37:24 +0000

    p3 complete

--- js3/3.js
+++ js3/3.js
@@ -6,6 +6,12 @@
 
 const solution = (arr) => {
   return (obj) => {
+      return arr.reduce( (acc, e) => {
+          if (obj.hasOwnProperty(e)) {
+              acc[e] = obj[e]
+          }
+          return acc
+      }, {})
   }
 }
 
