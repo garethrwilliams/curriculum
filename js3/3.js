d813f31ceec0a5deca7aa280a2a7c1c9d8ecb8f3
Gareth Williams <garethrwilliams@me.com>
2020-11-03 12:50:43 +0000

    p3 complete

--- js3/3.js
+++ js3/3.js
@@ -1,22 +1,10 @@
-/*
- * write a function called solution that takes in an array of strings and returns a function. when the returned function is called (with an object with many keys), it will return an object with only keys that exist in the input arrary.
- * @param {array} arr {object} obj
- * @return {object} obj
- */
-
- 
 const solution = (arr) => {
-  return (obj) => {
-      return arr.reduce( (acc, e) => {
-          if (obj.hasOwnProperty(e)) {
-              acc[e] = obj[e]
-          }
-          return acc
-      }, {})
+    return (obj) => {
+        return arr.reduce( (acc, e) => {
+            if (obj.hasOwnProperty(e)) {
+                acc[e] = obj[e]
+            }
+            return acc
+        }, {})
+    }
   }
-}
-
-
-module.exports = {
-  solution
-}
