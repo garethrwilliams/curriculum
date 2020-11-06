c643a0b50b6c646b7de57379d33988fca5014848
Gareth Williams <garethrwilliams@me.com>
2020-11-06 15:45:30 +0000

    complete

--- js3/7.js
+++ js3/7.js
@@ -4,17 +4,5 @@
  * @returns {number}
 */
 
-const solution = () => {
-  Object.prototype.map = function (cb, i=0, resultArray=[]) {
-      Object.keys(this).map( (key, i) => {
-        return cb(key, this[key], i)
-      })
-    }
-  }
-
-
-module.exports = {
-  solution 
-}
 
 
