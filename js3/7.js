89a5ff8a936df6f65420613d89abe0653f440454
Gareth Williams <garethrwilliams@me.com>
2020-11-06 12:16:51 +0000

    complete

--- js3/7.js
+++ js3/7.js
@@ -5,10 +5,13 @@
 */
 
 const solution = () => {
-  Object.prototype.map = function (cb) {
-    return []
+  Object.prototype.map = function (cb, i=0, resultArray=[]) {
+      if (Object.keys(this).length === i) return resultArray
+      resultArray.push(cb(Object.keys(this)[i], Object.values(this)[i], i))
+      return this.map(cb, i+1, resultArray)
   }
 }
+
 
 module.exports = {
   solution
